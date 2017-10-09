2e66c965a3bab4c3ee9455a0bbaea9f6e88ef1ab
Vasil <vsl.h.94@gmail.com>
2017-10-09 23:27:15 +0300

    Completed data aggregation exercises

--- Databases Basics - MS SQL Server/Data aggregation/Data aggregation - all solutions.sql
+++ Databases Basics - MS SQL Server/Data aggregation/Data aggregation - all solutions.sql
@@ -174,4 +174,15 @@ FROM
 ) AS RankedSalaries
 WHERE [Rank] = 3
 
+-- 19. Salary challenge
 
+SELECT TOP 10 FirstName, LastName, DepartmentID
+  FROM Employees AS emp1
+ WHERE Salary > 
+ (
+	SELECT AVG(Salary)
+	  FROM Employees AS emp2
+	 WHERE emp1.DepartmentID = emp2.DepartmentID
+	 GROUP BY DepartmentID
+ )
+ ORDER BY DepartmentID
