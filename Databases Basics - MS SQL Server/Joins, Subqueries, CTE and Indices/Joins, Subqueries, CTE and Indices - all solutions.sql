fcc504c322e83b16afec482c16f961afa1a1f35f
Vasil <vsl.h.94@gmail.com>
2017-10-13 15:36:33 +0300

    Completed Joins, Subqueries, CTE and Indices exercises

--- Databases Basics - MS SQL Server/Joins, Subqueries, CTE and Indices/Joins, Subqueries, CTE and Indices - all solutions.sql
+++ Databases Basics - MS SQL Server/Joins, Subqueries, CTE and Indices/Joins, Subqueries, CTE and Indices - all solutions.sql
@@ -173,5 +173,34 @@ ORDER BY MAX(p.Elevation) DESC,
          MAX(r.Length) DESC, 
 		 c.CountryName ASC
 
+-- 18. Highest peak name and elevation by country
 
-
+SELECT TOP 5
+       CountryName, 
+       CASE 
+	       WHEN PeakName IS NULL THEN ('no highest peak')
+		   ELSE PeakName
+	   END AS [Highest Peak Name], 
+	   CASE 
+	       WHEN Elevation IS NULL THEN 0
+		   ELSE Elevation
+	   END AS [Highest Peak Elevation], 
+	   CASE 
+	       WHEN MountainRange IS NULL THEN ('no mountain')
+		   ELSE MountainRange
+	   END AS [Mountain]
+FROM (
+	SELECT CountryName, PeakName, Elevation, MountainRange,
+	DENSE_RANK() OVER(PARTITION BY CountryName ORDER BY Elevation DESC) AS [Rank]
+	FROM ( 
+		SELECT c.CountryName,
+			   p.PeakName,
+			   p.Elevation,
+			   m.MountainRange
+		FROM Countries AS c
+		LEFT JOIN MountainsCountries AS mc ON mc.CountryCode = c.CountryCode
+		LEFT JOIN Mountains AS m ON m.Id = mc.MountainId
+		LEFT JOIN Peaks AS p ON p.MountainId = m.Id) AS TopPeaks) AS RankedPeaks
+		WHERE [Rank] = 1
+		ORDER BY CountryName,
+		         [Highest Peak Name]
