8da52f2fd09a22489293c1b67bfcfb519e44f677
Vasil <vsl.h.94@gmail.com>
2017-11-11 18:46:54 +0200

    Initial upload

---
+++ Databases Advanced - Entity Framework/Code First/HospitalStartUp/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace HospitalStartUp
+{
+    using P01_HospitalDatabase.Data;
+    using P01_HospitalDatabase.Initializer;
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            DatabaseInitializer.ResetDatabase();
+
+            //var db = new HospitalContext();
+
+            //using (db)
+            //{
+            //    DatabaseInitializer.InitialSeed(db);
+            //}
+        }
+    }
+}
+
---
+++ Databases Advanced - Entity Framework/Code First/P01_HospitalDatabase/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace P01_HospitalDatabase.Data
+{
+    public class Configuration
+    {
+        public static string ConnectionString { get; set; } = 
+            "Server=VASIL\\SQLEXPRESS;Database=Hospital;Integrated Security = true";
+    }
+}
