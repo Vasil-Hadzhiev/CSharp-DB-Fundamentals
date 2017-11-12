7f4cd81de2c66db62aee2b099af1dad056aa1de7
Vasil <vsl.h.94@gmail.com>
2017-11-12 18:45:55 +0200

    Completed exercises

---
+++ Databases Advanced - Entity Framework/Code First/SalesDB/P03_SalesDatabase/Migrations/20171112163558_Initial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace P03_SalesDatabase.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Stores");
+        }
+    }
+}
---
+++ Databases Advanced - Entity Framework/Code First/SalesDB/SalesStartUp/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace SalesStartUp
+{
+    using P03_SalesDatabase.Data;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var db = new SalesContext();
+
+            using (db)
+            {
+                db.Database.EnsureCreated();
+            }
+        }
+    }
+}
